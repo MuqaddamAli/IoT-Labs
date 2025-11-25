70e88ebbb6ffa7729b518a619c0a8b521da140e3
Muqaddam Ali <muqaddamali643@gmail.com>
2025-11-25 23:46:05 +0500

    Wifi - Week 9

---
+++ WiFi-Week9-Lecture2/src/main.cpp
@@ -0,0 +1,64 @@
+// // Connecting to a network //
+#include <WiFi.h>
+
+const char* ssid     = "";
+const char* password = "";
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+
+  Serial.println("Connecting to WiFi...");
+  WiFi.begin(ssid, password);   // Start connection
+
+  // Wait until connected
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println();
+  Serial.println("WiFi Connected!");
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  // Nothing here — stays connected
+}
+
+// // Scanning //
+// #include <WiFi.h>
+
+// void setup() {
+//   Serial.begin(115200);
+//   WiFi.mode(WIFI_STA);   // Station mode
+//   WiFi.disconnect();     // Not connected to any network
+//   delay(1000);
+// }
+
+// void loop() {
+//   Serial.println("Scanning WiFi networks...");
+
+//   int count = WiFi.scanNetworks();  // Scan request
+
+//   if (count <= 0) {
+//     Serial.println("No networks found");
+//   } else {
+//     Serial.print("Found ");
+//     Serial.print(count);
+//     Serial.println(" networks:");
+//   }
+
+//   for (int i = 0; i < count; i++) {
+//     Serial.print(i + 1);
+//     Serial.print(": ");
+//     Serial.print(WiFi.SSID(i));
+//     Serial.print(" (");
+//     Serial.print(WiFi.RSSI(i));
+//     Serial.println(" dBm)");
+//   }
+
+//   Serial.println();
+//   delay(5000);   // Scan every 5 seconds
+// }
